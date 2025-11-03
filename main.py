8a231714e85b1b297c0ef62f7988c693829eabed
Renato-M-Silva <116196979+Renato-M-Silva@users.noreply.github.com>
2025-11-03 12:34:10 +0000

    Update main.py
    Doing some final adjustments to make the code cleaner.

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ import csv
 import sys
 import threading
 
-"""Finds the background color of an image and returns whether it is light or dark."""
+# Finds the background color of an image and returns whether it is light or dark.
 def find_background_color(image):
+    """Finds the background color of an image and returns whether it is light or dark."""
     # Resize the image to speed up calculation (optional)
     image = cv2.resize(image, (200, 200))
 
@@ -29,8 +30,9 @@ def find_background_color(image):
     else:
         return "light"
 
-"""Opens a file selection dialog and returns a list with the paths of the selected files."""
+# Opens a file selection dialog and returns a list with the paths of the selected files.
 def select_files():
+    """Opens a file selection dialog and returns a list with the paths of the selected files."""
     root = tk.Tk()  # Create a Tkinter window
     root.withdraw()  # Hide the main window
 
@@ -40,8 +42,9 @@ def select_files():
 
     return list(file_paths)  # Convert the returned tuple to a list
 
-"""Adds the paths of the selected files to the list."""
+# Adds the paths of the selected files to the list.
 def add_files_to_list(file_list):
+    """Adds the paths of the selected files to the list."""
     selected_file_paths = select_files()
 
     if selected_file_paths:  # Check if the user selected any files
@@ -52,8 +55,9 @@ def add_files_to_list(file_list):
     else:
         print("No files selected.")
 
-"""Pre-process the image and find the contours"""
+# Pre-process the image and find the contours
 def preprocess(image_to_process, background_color):
+    """Pre-process the image and find the contours"""
     # changes based on image been lither or darker:
     #  in grayscale we have 41 ± 5 for darker images and 244 ± 5 for lighter ones
     #  in contour detection block_size = 11 / c_constant = 11 for darker  and block_size =  / c_constant =  for ligther images
@@ -89,8 +93,9 @@ def preprocess(image_to_process, background_color):
 
     return contours
 
-"""Find horizontal lines to divide the image and returns a list"""
+# Find horizontal lines to divide the image and returns a list
 def find_lines(contours):
+    """Find horizontal lines to divide the image and returns a list"""
     # Filter horizontal contours
     horizontal_lines = []
     for contour in contours:
@@ -105,8 +110,9 @@ def find_lines(contours):
                 horizontal_lines.append(approx)
     return horizontal_lines
 
-"""Calculate distances between lines"""
+# Calculate distances between lines
 def find_distances(horizontal_lines):
+    """Calculate distances between lines"""
     distances = []
     if len(horizontal_lines) > 1:
         for i in range(1, len(horizontal_lines)):
@@ -124,8 +130,9 @@ def find_distances(horizontal_lines):
 
     return distances
 
-"""Function to create subimages"""
+# Function to create subimages
 def create_subimages(image_to_process, horizontal_lines):
+    """Function to create subimages"""
     horizontal_lines.reverse()
     subimages = []
     previous_height = 0
@@ -138,8 +145,9 @@ def create_subimages(image_to_process, horizontal_lines):
         previous_height = current_height
     return subimages
 
-"""Reads the text in the images and returns a list of strings"""
+# Reads the text in the images and returns a list of strings
 def read_subimages(subimages):
+    """Reads the text in the images and returns a list of strings"""
     image_text = []
     reader = easyocr.Reader(['pt', 'en'])  # Initialize EasyOCR with the specified language
     for subimage in subimages:
@@ -147,13 +155,15 @@ def read_subimages(subimages):
         image_text.append(' '.join(text))  # Join the text parts into a single string
     return image_text
 
-"""Remove all elements from the list that contain the words that shows that the image was not a data or an uber trip, for exemplo the word 'Histórico'"""
+# Remove all elements from the list that contain the words that shows that the image was not a data or an uber trip, for exemplo the word 'Histórico'
 def remove_values(text_list):
+    """Remove all elements from the list that contain the words that shows that the image was not a data or an uber trip, for exemplo the word 'Histórico'"""
     words_to_remove = ['Histórico', 'durante', 'Recurso']
     return [text for text in text_list if not any(word in text for word in words_to_remove)]
 
-"""Displays a loading animation in the console until a stop event is signaled."""
+# Displays a loading animation in the console until a stop event is signaled.
 def loading_animation(stop_event):
+    """Displays a loading animation in the console until a stop event is signaled."""
     chars = "/-\|"
     while not stop_event.is_set():
         for char in chars:
@@ -172,7 +182,7 @@ if __name__ == "__main__":
 
     print("------------------------------------")
 
-    # Starting to read the images:
+    # Starting to load the images and doing transformations to then save in CSV format:
     for image_file_path in my_list:
         print(f'Getting the data from image: {image_file_path}.')
         image_to_process = cv2.imread(image_file_path)
@@ -181,38 +191,9 @@ if __name__ == "__main__":
         background_color = find_background_color(image_to_process)
         print(f"The image has a {background_color} background.")
 
-        ### Calling functions ###
+        # Calling functions to pre-process images 
 
         contours = preprocess(image_to_process, background_color)
-
-        """ # testing contours. if needed, Activate this code by uncommenting it.
-        lower_gray = 239
-        upper_gray = 249
-        # Grayscale
-        gray_image = cv2.cvtColor(image_to_process, cv2.COLOR_BGR2GRAY)
-        # Mask to isolate gray tones
-        mask = cv2.inRange(gray_image, lower_gray, upper_gray)
-        # Apply mask
-        filtered_image = cv2.bitwise_and(gray_image, gray_image, mask=mask)
-        
-        # Preprocessing for contour detection
-        for block_size in [5, 7, 9]:
-            for c_constant in [2, 5]:
-                thresh = cv2.adaptiveThreshold(filtered_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, block_size, c_constant)
-                # Visualizar a imagem binária
-                cv2.imshow(f"blockSize: {block_size}, c: {c_constant}", thresh[32000:32500, :])
-                kernel = np.ones((3, 3), np.uint8)
-                thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-                # Contour detection
-                contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                test_image = image_to_process.copy()
-                for contour in contours:
-                    cv2.drawContours(test_image, [contour], 0, (0, 255, 0), 2)
-                cv2.imshow(f'test_image0-blockSize: {block_size}, c: {c_constant}', test_image[000:500, :])
-                cv2.imshow(f'test_image1-blockSize: {block_size}, c: {c_constant}', test_image[32000:32500, :])
-        cv2.waitKey(0)
-        """
-        
         print("Contours - ok!")
 
         horizontal_lines = find_lines(contours)
@@ -224,6 +205,7 @@ if __name__ == "__main__":
         subimages = create_subimages(image_to_process, horizontal_lines)
         print("Subimages - ok!")
 
+        # starting EasyOCR processing with GPU monitoring to read text from subimages
         start_time = time.time() # Monitor GPU usage during EasyOCR processing
         process = subprocess.Popen(['nvidia-smi', '-l', '1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # Start nvidia-smi in a subprocess
         print("Nvidia-smi started in subprocess - ok!")
@@ -239,6 +221,7 @@ if __name__ == "__main__":
         print(f"\rEasyOCR processing time: {end_time - start_time:.2f} seconds")
         print("Reading images - ok!")
 
+        # Remove unwanted values from the extracted text, to add more values add to the list in the function
         image_text = remove_values(image_text)
         print("Removing values - ok!")
 
@@ -255,58 +238,5 @@ if __name__ == "__main__":
 
         print(f"Data saved to {csv_file_path}")
         print("------------------------------------")
-
-        """ # Testing . if needed, Activate this code by uncommenting it.
-        print("------------------------------------")
-        print("Testing...")
-        # Print total number of lines
-        print(len(horizontal_lines))
-
-        # Show contours and lines on copies of the image
-        # Draw the found contours on a copy of the image
-        test_image = image_to_process.copy()
-        for contour in contours:
-            cv2.drawContours(test_image, [contour], 0, (0, 255, 0), 2)
-        cv2.imshow('test_image', test_image[:, :])
-
-        # Create a copy of the original image to draw the lines
-        image_with_lines = image_to_process.copy()
-        # Draw the detected lines and add the line number and distance to the previous line
-        for i, line in enumerate(horizontal_lines):
-            x1, y1 = line[0][0]
-            x2, y2 = line[1][0]
-            # Print line on the image
-            cv2.line(image_with_lines, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            if i > 0:
-                distance = distances[i - 1]
-                text = f"Line: {i}, Distance: {distance}"
-                # Adjust the text position to be more centered and readable
-                text_x = (x1 + 15)
-                # Adjust the vertical position of the text
-                text_y = y1 + 15
-                # Print text of line number and distance
-                cv2.putText(image_with_lines, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-        cv2.imshow('image_with_lines', image_with_lines[:, :])
-
-
-        # display the first 10 subimages
-        for i in range(min(10, len(subimages))):  # Limit to 10 or the total number of subimages, whichever is less
-            try:
-                cv2.imshow(f"subimage_{i}", subimages[i])
-            except cv2.error as e:
-                print(f"Error displaying subimage {i}: {e}")
-
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-        # Save image with lines
-        cv2.imwrite("lines.png", image_with_lines)
-
-        # Print the first 10 lines of text
-        for i in range(min(10, len(image_text))):
-            print(f"Text from subimage {i + 1}: {image_text[i]}")
-    """
-
 
     print("The process ended.")
